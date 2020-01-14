e391c508383e9a6e84991f52bcac5562a5076567
Antoine Grigis <antoine.grigis@cea.fr>
2020-01-14 16:29:18 +0100

    pyfuntk/scripts/pyfuntk_spm_applywarp: add script to apply SPM warp.

--- pyfuntk/preproc/registration.py
+++ pyfuntk/preproc/registration.py
@@ -16,6 +16,7 @@ import nibabel
 from pyfuntk.stats.utils import recursive_gzip
 from pyfuntk.stats.utils import ungzip_file
 from pyfuntk import DEFAULT_SPM_STANDALONE_PATH
+from pyfuntk.preproc.interfaces import ApplyDeformationField
 
 # Nipype import
 from nipype.interfaces import spm
@@ -208,5 +209,137 @@ def spm_tissue_probability_maps(tpm_file):
     tissue5 = ((tpm_file, 5), 4, (False, False), (False, False))
     tpm_struct = [tissue1, tissue2, tissue3, tissue4, tissue5]
     return tpm_struct
+
+
+def spm_applywarp(moving_file, reference_file, deformation_file, outdir,
+                  interp=1, inverse=False, spmbin=DEFAULT_SPM_STANDALONE_PATH):
+    """ Apply an SPM deformation filed to a volume.
+
+    Parameters
+    ----------
+    moving_file: str
+        the file to normalize.
+    reference_file: str
+        the file defing the target space.
+    deformation_file: str
+        the deformation field.
+    outdir: str
+        the destination folder.
+    interp: int, default 1
+        degree of b-spline used for interpolation.
+    inverse: bool, deafult False
+        apply the inverse deformation field.
+    spmbin: str, default DEFAULT_SPM_STANDALONE_PATH
+        the SPM standalone file.
+
+    Returns
+    -------
+    normalize_file: str
+        the normalized file.
+    """
+    # Configuration
+    spm.SPMCommand.set_mlab_paths(
+        matlab_cmd="{0} script ".format(spmbin), use_mcr=True)
+    cwd = os.getcwd()
+    os.chdir(outdir)
+
+    # Call interface
+    interface = ApplyDeformationField(
+        in_files=moving_file,
+        deformation_field=deformation_file,
+        reference_file=reference_file,
+        savedir=outdir,
+        interpolation=interp)
+    runtime = interface.run()
+    applywarp_outputs = runtime.outputs.get()
+    normalize_file = applywarp_outputs["normalized_files"]
+
+    return normalize_file
+
+
+def spm_dartel(gmfiles, wmfiles, outdir, spmbin=DEFAULT_SPM_STANDALONE_PATH):
+    """ SPM DARTEL.
+
+    Parameters
+    ----------
+    gmfiles: list of str
+        the DARTEL input gray matter files 'rc1' for each subject.
+    wmfiles: list of str
+        the DARTEL input white matter files 'rc2' in the same order.
+    outdir: str
+        the destination folder.
+    spmbin: str (optional, default DEFAULT_SPM_STANDALONE_PATH)
+        the SPM standalone file.
+
+    Returns
+    -------
+    dartel_flow_fields: list of str
+        DARTEL flow fields.
+    template_files: list of str
+        templates from different stages of iteration.
+    final_template_file: str
+        final DARTEL template.
+    """
+    # Configuration
+    spm.SPMCommand.set_mlab_paths(
+        matlab_cmd="{0} script ".format(spmbin), use_mcr=True)
+    cwd = os.getcwd()
+    os.chdir(outdir)
+
+    # Compute DARTEL template for group data and associated flow fields
+    interface = spm.DARTEL(
+        image_files=[gmfiles, wmfiles])
+    runtime = interface.run()
+    dartel_outputs = runtime.outputs.get()
+    dartel_flow_fields = dartel_outputs["dartel_flow_fields"]
+    template_files = dartel_outputs["template_files"]
+    final_template_file = dartel_outputs["final_template_file"]
+
+    return dartel_flow_fields, template_files, final_template_file
+
+
+def spm_apply_dartel(files, template_file, flowfield_files, outdir,
+                     spmbin=DEFAULT_SPM_STANDALONE_PATH):
+    """ Apply SPM DARTEL.
+
+    Parameters
+    ----------
+    files: list of str
+        the files in native space to be warped.
+    template_file: str
+        the DARTEL template.
+    flowfield_files: list of str
+        the DARTEL flow field to be applied ('u_rc1*')
+    outdir: str
+        the destination folder.
+    spmbin: str (optional, default DEFAULT_SPM_STANDALONE_PATH)
+        the SPM standalone file.
+
+    Returns
+    -------
+    normalized_files: list of str
+        the warped input files.
+    normalization_parameter_file:
+        transform parameters to MNI space.
+    """
+    # Configuration
+    spm.SPMCommand.set_mlab_paths(
+        matlab_cmd="{0} script ".format(spmbin), use_mcr=True)
+    cwd = os.getcwd()
+    os.chdir(outdir)
+
+    # Compute DARTEL template for group data and associated flow fields
+    interface = spm.DARTELNorm2MNI(
+        template_file=template_file,
+        flowfield_files=flowfield_files,
+        apply_to_files=files,
+        modulate=True,
+        fwhm=[8, 8, 8])
+    runtime = interface.run()
+    dartel_outputs = runtime.outputs.get()
+    normalized_files = dartel_outputs["normalized_files"]
+    normalization_parameter_file = dartel_outputs["normalization_parameter_file"]
+
+    return normalized_files, normalization_parameter_file
 
 
